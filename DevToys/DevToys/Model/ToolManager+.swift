96e7af0b174d0fef23945fb324da37696b79900a
Obuchi Yuki <yukibochi1@gmail.com>
2022-02-13 10:53:58 +0900

    add Tool Manager

--- DevToys/DevToys/Model/ToolManager+.swift
+++ DevToys/DevToys/Model/ToolManager+.swift
@@ -6,20 +6,38 @@
 //
 
 import CoreUtil
+import OrderedCollections
 
 final class ToolManager {
     
     private var toolMap = [ToolCategory?: [Tool]]()
+    private var categoryOrder = OrderedSet<ToolCategory?>()
     
     func registerTool(_ tool: Tool) {
-        self.toolMap[tool.category]
-        
-        tool.category
+        self.toolMap.arrayAppend(tool, forKey: tool.category)
+        if !categoryOrder.contains(tool.category) { categoryOrder.append(tool.category) }
+    }
+    
+    func allToolCategories(_ query: Query) -> [ToolCategory?] {
+        categoryOrder.filter{ query.matches(to: $0?.name ?? "") }
     }
 }
 
-struct ToolCategory: Hashable {
+final class ToolCategory: Hashable {
     let name: String
+    let identifier: String
+    
+    static func == (lhs: ToolCategory, rhs: ToolCategory) -> Bool {
+        lhs.identifier == rhs.identifier
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+    init(name: String, identifier: String) {
+        self.name = name
+        self.identifier = identifier
+    }
 }
 
 final class Tool {
