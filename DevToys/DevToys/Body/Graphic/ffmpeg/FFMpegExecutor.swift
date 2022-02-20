3d071ccad10da2fa3f378322a3609a18b9c0fa5d
Obuchi Yuki <yukibochi1@gmail.com>
2022-02-20 16:15:29 +0900

    fix Progress

--- DevToys/DevToys/Body/Graphic/ffmpeg/FFMpegExecutor.swift
+++ DevToys/DevToys/Body/Graphic/ffmpeg/FFMpegExecutor.swift
@@ -40,30 +40,57 @@ enum FFMpegExecutor {
 
 
 public final class Progress<Failure: Error> {
-    public enum Status {
+    public enum State {
         case progressing(Double)
-        case failed(Failure)
+        case rejected(Failure)
         case completed
     }
     
-    public let taskCount: Int
-    public var completedTaskCount = 0
-    public var status: Status = .progressing(0)
-    
-    public func complete(_ count: Int) {
-        guard case .progressing = status else { return }
-        
-        self.completedTaskCount = min(taskCount, completedTaskCount + count)
-        
-        if self.completedTaskCount == self.taskCount {
-            self.status = .fullfill
-        } else {
-            self.status = .progressing(Double(completedTaskCount) / Double(taskCount))
-        }
+    struct Subscriber {
+        let progress: (Double) -> ()
+        let complete: () -> ()
+        let reject: (Failure) -> ()
     }
+    
+    public let taskCount: Int
+    public private(set) var completedTaskCount = 0
+    public private(set) var state: State = .progressing(0)
+    
+    var subscribers = [Subscriber]()
     
     public init(taskCount: Int) {
         assert(taskCount > 0)
         self.taskCount = taskCount
+    }
+        
+    public func reject(_ failure: Failure) {
+        guard case .progressing = state else { return }
+        self.state = .rejected(failure)
+        for subscriber in subscribers { subscriber.reject(failure) }
+        self.subscribers.removeAll()
+    }
+    
+    public func progress(_ count: Int) {
+        guard case .progressing = state else { return }
+        
+        self.completedTaskCount = min(taskCount, completedTaskCount + count)
+        
+        if self.completedTaskCount == self.taskCount {
+            self.state = .completed
+            for subscriber in subscribers { subscriber.complete() }
+            self.subscribers.removeAll()
+        } else {
+            let progress = Double(completedTaskCount) / Double(taskCount)
+            self.state = .progressing(progress)
+            for subscriber in subscribers { subscriber.progress(progress) }
+        }
+    }
+    
+    func subscribe(_ resolve: @escaping (Output) -> (), _ reject: @escaping (Failure) -> ()) {
+        switch self.state {
+        case .pending: self.subscribers.append(Subscriber(resolve: resolve, reject: reject))
+        case .fulfilled(let output): resolve(output)
+        case .rejected(let failure): reject(failure)
+        }
     }
 }
